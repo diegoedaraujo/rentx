53bf367b8830597612e6dfe1add7ba1157a35757
Diego Araújo <diegoedaraujo@gmail.com>
2022-01-07 10:48:50 -0300

    Remoção do campo username da tabela users

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -4,11 +4,10 @@ import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { name, username, email, password, driver_licence } = request.body;
+    const { name, email, password, driver_licence } = request.body;
     const createUserUseCase = container.resolve(CreateUserUseCase);
     await createUserUseCase.execute({
       name,
-      username,
       email,
       password,
       driver_licence,
