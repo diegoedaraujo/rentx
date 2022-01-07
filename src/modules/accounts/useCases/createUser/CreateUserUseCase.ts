7b65ea0ee2a9c31fb5ed03a520505e48fe6bb3aa
Diego Araújo <diegoedaraujo@gmail.com>
2022-01-07 11:41:45 -0300

    Add bcryptjs

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcryptjs";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -15,10 +16,12 @@ class CreateUserUseCase {
     password,
     driver_licence,
   }: ICreateUserDTO): Promise<void> {
+    const salt = 8;
+    const passwordHash = await hash(password, salt);
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_licence,
     });
   }
