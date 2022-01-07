46d05be2c9a9f35901b483298b18dc1ba55b68bf
Diego Araújo <diegoedaraujo@gmail.com>
2022-01-07 11:54:44 -0300

    Alterando user

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -16,6 +16,11 @@ class CreateUserUseCase {
     password,
     driver_licence,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
     const salt = 8;
     const passwordHash = await hash(password, salt);
     await this.usersRepository.create({
