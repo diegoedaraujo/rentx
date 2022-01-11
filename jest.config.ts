e978b01eef80bc97fad6aef323dd78b119d1b0db
Diego Araújo <diegoedaraujo@gmail.com>
2022-01-11 16:33:52 -0300

    Finalizando configurações do jest

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,8 @@ export default {
   // automock: false,
 
   // Stop running tests after `n` failures
-  // bail: 0,
+  // valor 0 para retornar no final
+  bail: true,
 
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "C:\\Users\\Diego Araujo\\AppData\\Local\\Temp\\jest",
@@ -146,10 +147,7 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: ["**/*.spec.ts"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
