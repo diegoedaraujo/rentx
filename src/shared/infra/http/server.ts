ea68bad2a94119ee01f0ce0dae90b14c591a409a
Diego Araújo <diegoedaraujo@gmail.com>
2022-01-12 13:56:49 -0300

    Refatorando a aplicação

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,11 +1,11 @@
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
-import "./database";
+import "../typeorm";
 import "@shared/container";
 import { router } from "./routes";
-import swaggerDocument from "./swagger.json";
-import { AppError } from "@errors/AppError";
+import swaggerDocument from "../../../swagger.json";
+import { AppError } from "@shared/errors/AppError";
 
 const app = express();
 app.use(express.json());
