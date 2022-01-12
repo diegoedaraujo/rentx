8fb0ac7845b52b7e76f78eba2059388ec8c9ab63
Diego Araújo <diegoedaraujo@gmail.com>
2022-01-12 13:22:59 -0300

    Corrigindo as importações

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUserCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUserCase.spec.ts
@@ -1,6 +1,6 @@
-import { AppError } from "../../../../errors/AppError";
-import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
-import { UserReposirotyInMemory } from "../../repositories/in-memory/UserRepositoryInMemory";
+import { AppError } from "@errors/AppError";
+import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
+import { UserReposirotyInMemory } from "@modules/accounts/repositories/in-memory/UserRepositoryInMemory";
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
 
