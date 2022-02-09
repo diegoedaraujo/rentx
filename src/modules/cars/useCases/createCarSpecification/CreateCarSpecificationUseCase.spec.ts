c9d28c6d10d0a60dc3dceb8d16b12468f4c589f6
Diego Araújo <diegoedaraujo@gmail.com>
2022-02-09 16:44:58 -0300

    Fix testes

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 import { CreateCarSpecificationUseCase } from "./CreateCarSpecificationUseCase";
 
 let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
@@ -12,9 +13,27 @@ beforeEach(() => {
 });
 
 describe("Create Car Specification", () => {
+  it("Should not be able to add a new specification to a now-existent car", async () => {
+    expect(async () => {
+      const car_id = "1234";
+      const specification_id = ["54321"];
+      await createCarSpecificationUseCase.execute({ car_id, specification_id });
+    }).rejects.toBeInstanceOf(AppError);
+  });
   it("Should be able to add a new specification to the car", async () => {
-    const car_id = "1234";
+    const car = await carsRepositoryInMemory.create({
+      name: "Name Car",
+      description: "Description Car",
+      daily_rate: 100,
+      licence_plate: "ABC-1234",
+      fine_amount: 60,
+      brand: "Brand",
+      category_id: "category",
+    });
     const specification_id = ["54321"];
-    await createCarSpecificationUseCase.execute({ car_id, specification_id });
+    await createCarSpecificationUseCase.execute({
+      car_id: car.id,
+      specification_id,
+    });
   });
 });
