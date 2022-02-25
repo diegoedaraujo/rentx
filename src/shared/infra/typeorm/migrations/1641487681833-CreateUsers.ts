248168f8776d36f9aab3af833cb32521b048a050
Diego Araújo <diegoedaraujo@gmail.com>
2022-02-25 11:27:03 -0300

    Criando migrations rentals

--- src/shared/infra/typeorm/migrations/1641487681833-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1641487681833-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1641487681833 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
