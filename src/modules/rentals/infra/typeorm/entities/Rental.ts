dabfc8867c6a91ddf0484170c6c34bb655c3921a
Diego Araújo <diegoedaraujo@gmail.com>
2022-03-02 14:21:09 -0300

    Implementando rental

---
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -0,0 +1,18 @@
+import { v4 as uuidV4 } from "uuid";
+
+class Rental {
+  id: string;
+
+  car_id: string;
+
+  user_id: string;
+
+  start_date: Date;
+
+  end_date: Date;
+
+  expected_return_date:Date;
+
+  total: number;
+}
+export { Rental };
