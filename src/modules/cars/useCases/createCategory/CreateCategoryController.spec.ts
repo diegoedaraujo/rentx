60181f16dfbc7500bfde8c30e5e4161f2bba42cd
Diego Araújo <diegoedaraujo@gmail.com>
2022-03-04 11:17:57 -0300

    Implementando testes de integração

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,7 +1,7 @@
 import { app } from "@shared/infra/http/app";
 import request from "supertest";
 
-describe("Create Category Controller", async () => {
+describe("Create Category Controller", () => {
   it("Should be able to create a new category", async () => {
     const response = await request(app).post("/categories").send({
       name: "Category Supertest",
