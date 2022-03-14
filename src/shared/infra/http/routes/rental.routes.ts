52925ee00c71778bcf637133065980dfdec7738c
Diego Araújo <diegoedaraujo@gmail.com>
2022-03-14 16:44:45 -0300

    Implementando listagem de aluguéis

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,17 +1,24 @@
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const rentalRouters = Router();
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRouters.post("/", ensureAuthenticated, createRentalController.handle);
 rentalRouters.post(
   "/devolution/:id",
   ensureAuthenticated,
   devolutionRentalController.handle
+);
+rentalRouters.get(
+  "/user",
+  ensureAuthenticated,
+  listRentalsByUserController.handle
 );
 
 export { rentalRouters };
