841ba406a4c984eb59043ddc1dd13505507e839c
Diego Araújo <diegoedaraujo@gmail.com>
2022-03-23 11:30:35 -0300

    Implementando refresh_token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "4c0cafd1773500edf30a69a2f7be8753",
+  expires_in_token: "15m",
+  secret_refresh_token: "4d6b14b59dc3a3e737bf6ca3baa54740",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -3,6 +3,9 @@ import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepositor
 import { compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
 import { AppError } from "@shared/errors/AppError";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import auth from "@config/auth";
+import { IDateProvider } from "@shared/container/providers/DayjsProvider/IDateProvider";
 
 interface IRequest {
   email: string;
@@ -14,15 +17,27 @@ interface IResponse {
     email: string;
   };
   token: string;
+  refresh_token: string;
 }
 @injectable()
 class AuthenticateUserUseCase {
   constructor(
     @inject("UsersRepository")
-    private userRepository: IUsersRepository
+    private userRepository: IUsersRepository,
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider
   ) {}
   async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.userRepository.findByEmail(email);
+    const {
+      expires_in_token,
+      secret_token,
+      secret_refresh_token,
+      expires_in_refresh_token,
+      expires_refresh_token_days,
+    } = auth;
     const msgError = "Email or password incorrect!";
     if (!user) {
       throw new AppError(msgError);
@@ -31,11 +46,23 @@ class AuthenticateUserUseCase {
     if (!passwordMatch) {
       throw new AppError(msgError);
     }
-    const token = sign({}, "4c0cafd1773500edf30a69a2f7be8753", {
+    const token = sign({}, secret_token, {
       subject: user.id,
-      expiresIn: "1d",
+      expiresIn: expires_in_token,
     });
-    return { user, token };
+    const refresh_token = sign({ email }, secret_refresh_token, {
+      subject: user.id,
+      expiresIn: expires_in_refresh_token,
+    });
+    const refresh_token_expires_date = this.dateProvider.addDays(
+      expires_refresh_token_days
+    );
+    await this.usersTokensRepository.create({
+      user_id: user.id,
+      refresh_token,
+      expires_date: refresh_token_expires_date,
+    });
+    return { user, token, refresh_token };
   }
 }
 
