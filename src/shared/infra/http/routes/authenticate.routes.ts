8cc6e3e3dbd5aa535a148362e8feac6bf209ab85
Diego Araújo <diegoedaraujo@gmail.com>
2022-03-23 15:16:42 -0300

    Alterando lógica de autenticação

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserContoller";
+import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
 
 const authenticateRouters = Router();
 
 const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
+
 authenticateRouters.post("/sessions", authenticateUserController.handle);
+authenticateRouters.post("/refresh-token", refreshTokenController.handle);
 
 export { authenticateRouters };
